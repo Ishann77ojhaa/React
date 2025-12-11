f8d08d5b996812e961cd2c302bb796b38820e83c
Ishann Ojhaa <ishannojhaa@gmail.com>
2025-12-11 12:08:12 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  const Mycss = {
+    color : "blue"
+  }
+
+  return (
+    <div>
+      <h1 style={{color:'grey'}}>Ishann Ojhaa</h1>
+      <h2>Siuuuuu</h2>
+      <h2 style = {Mycss}>Chemical Formula of Water: H<sub>2</sub>O</h2>
+      <h2>11 + 11: {11 +11}</h2>
+
+    </div>
+  )
+}
+
+export default App
